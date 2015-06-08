27531dd641861e9aec76854221ed7cc7c0a5018b
Alexis Martin <martinalexis90@gmail.com>
2015-06-08 13:15:01 +0200

    test hyperMultiGraph

--- repository.lua
+++
@@ -1,4 +0,0 @@
---repository
-
-local repository = {}
-return repository
--- tests.lua
+++ tests.lua
@@ -22,47 +22,53 @@
 --end
 
 
-
-function createMultiGraph()
-	local repository = require "repository"
-	repository.hyperMultiGraph = require "hyper-multi-graph"
-	repository.multi_graph = require "multi-graph"
-
-	function repository.addNode(v, k)
-		repository.hyperMultiGraph.vertices[k] = v
-	end
-	
-	function repository.addEdge(node1, node2, k)
-		repository.hyperMultiGraph.edges[k] = {}
-		repository.hyperMultiGraph.edges[k].node1 = node1
-		repository.hyperMultiGraph.edges[k].node2 = node2
-		print(repository.multi_graph.edge_type.check())
-	end
-	
-	function repository.toString()
-		print("[vertices : ")
-		for k,v in pairs(repository.hyperMultiGraph.vertices) do
-			print(k .. " = " .. v .. "; ")
-		end
-		print("]\n")
-		
-		print("[edges : ")
-		for k,v in pairs(repository.hyperMultiGraph.edges) do
-			print("(" .. v.node1 .. ", " .. v.node2 .. "); ")
-		end
-		print("]\n")
-	end
-	
+local function createHyperMultiGraph()
+	repository = require "hyper-multi-graph"
 	return repository
 end
 
+
+
+--function createMultiGraph()
+
+--	repository.hyperMultiGraph = require "hyper-multi-graph"
+--	repository.multi_graph = require "multi-graph"
+
+--	function repository.addNode(v, k)
+--		repository.hyperMultiGraph.vertices[k] = v
+--	end
+--	
+--	function repository.addEdge(node1, node2, k)
+--		repository.hyperMultiGraph.edges[k] = {}
+--		repository.hyperMultiGraph.edges[k].node1 = node1
+--		repository.hyperMultiGraph.edges[k].node2 = node2
+--		print(repository.multi_graph.edge_type.check())
+--	end
+--	
+--	function repository.toString()
+--		print("[vertices : ")
+--		for k,v in pairs(repository.hyperMultiGraph.vertices) do
+--			print(k .. " = " .. v .. "; ")
+--		end
+--		print("]\n")
+--		
+--		print("[edges : ")
+--		for k,v in pairs(repository.hyperMultiGraph.edges) do
+--			print("(" .. v.node1 .. ", " .. v.node2 .. "); ")
+--		end
+--		print("]\n")
+--	end
+--	
+--	return repository
+--end
+
 do
-	local multi_graph = createMultiGraph()
-	multi_graph.addNode("nil", 1) 
-	multi_graph.addNode("nil", 2)
-	multi_graph.addNode(3, 1)
-	multi_graph.addEdge(1, 2, 1)
-	multi_graph.toString()
+	local multi_graph = createHyperMultiGraph()
+--	multi_graph.addNode("nil", 1) 
+--	multi_graph.addNode("nil", 2)
+--	multi_graph.addNode(3, 1)
+--	multi_graph.addEdge(1, 2, 1)
+--	multi_graph.toString()
 end	
 	
 	
