001516783a1fb76857bba558e6a1feb0ea746b1b
Alban Linard <alban@linard.fr>
2016-01-28 15:55:38 +0100

    Add function to check the container of a value.

---
+++ src/formalism/data/check_container.lua
@@ -0,0 +1,19 @@
+local Layer = require "layeredata"
+
+return function (value, container, where)
+  if getmetatable (value) ~= Layer.Proxy then
+    Layer.coroutine.yield (where.prefix .. ":not-proxy", {
+      proxy    = where.proxy,
+      key      = where.key,
+      expected = container,
+      used     = type (value),
+    })
+  elseif not Layer.Proxy.is_prefix (container, value) then
+    Layer.coroutine.yield (where.prefix .. ":not-contained", {
+      proxy    = where.proxy,
+      key      = where.key,
+      expected = container,
+      used     = value,
+    })
+  end
+end
