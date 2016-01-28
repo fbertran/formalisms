1bd8c81e398eeaf15e6867d2a61a4f9a9d039b6b
Alban Linard <alban@linard.fr>
2016-01-28 15:55:24 +0100

    Add function to check the type of a value.

---
+++ src/formalism/data/check_type.lua
@@ -0,0 +1,40 @@
+local Layer = require "layeredata"
+
+return function (value, oftype, where)
+  if value == nil then
+    Layer.coroutine.yield (where.prefix .. ":missing", {
+      proxy    = where.proxy,
+      key      = where.key,
+      expected = getmetatable (oftype) == Layer.Proxy
+             and oftype
+              or type (oftype),
+    })
+  elseif type (oftype) == "string" then
+    if type (value) ~= oftype then
+      Layer.coroutine.yield (where.prefix .. ":illegal", {
+        proxy    = where.proxy,
+        key      = where.key,
+        expected = type (oftype),
+        used     = type (value),
+      })
+    end
+  elseif getmetatable (oftype) == Layer.Proxy then
+    if getmetatable (value) ~= Layer.Proxy then
+      Layer.coroutine.yield (where.prefix .. ":illegal", {
+        proxy    = where.proxy,
+        key      = where.key,
+        expected = oftype,
+        used     = type (value),
+      })
+    elseif not (oftype <= value) then
+      Layer.coroutine.yield (where.prefix .. ":illegal", {
+        proxy    = where.proxy,
+        key      = where.key,
+        expected = oftype,
+        used     = value,
+      })
+    end
+  else
+    assert (false)
+  end
+end
