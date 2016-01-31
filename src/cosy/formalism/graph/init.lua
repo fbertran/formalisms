cddc33d3ff54582411f2db09cc492d532d1230ed
Alban Linard <alban@linard.fr>
2016-01-31 21:35:31 +0100

    Update and comment the graph formalism.

--- src/cosy/formalism/graph/init.lua
+++ src/cosy/formalism/graph/init.lua
@@ -1,58 +1,82 @@
 local Layer      = require "layeredata"
-local collection = require "formalisms.collection"
-local record     = require "formalisms.record"
+local collection = require "cosy.formalism.data.collection"
+local record     = require "cosy.formalism.data.record"
 local layer      = Layer.new {
-  name = "hyper & multi graph",
+  name = "graph (hyper & multi)",
 }
-local _          = Layer.reference "HMGT"
 
--- Formalism of Hyper and Multi Graph
--- ==================================
+-- Graph
+-- =====
 --
--- We describe here Hyper and Multi Graph
+-- Base graph structure for all graph-based formalisms.
+-- This formalism defines a special kind of graphs, the most generic we could
+-- find: hyper multi graphs.
 --
--- An Hyper and Multi Graph is a set V of vertices and a set E of edges. Each edge is a subset of V. In this formalism some edges may be identical.
---
--- For more information of Hyper and Multi Graph, see [here](https://en.wikipedia.org/?title=Hypergraph)
+-- For more information of Hyper and Multi Graph, see
+-- [hypergraph](https://en.wikipedia.org/wiki/Hypergraph) and
+-- [multigraph](https://en.wikipedia.org/wiki/Multigraph).
 
-layer[Layer.key.labels] = { HMGT = true }
 
-layer[Layer.key.meta] = {
-  vertex_type = {},
+-- For me: Petri net containers should set their graph container to refine them!
+-- This inversion allows to create sub-containers with specific defaults,
+-- one for places, one for transitions... and the graph container for all vertices.
 
-  edge_type = {
-    [Layer.key.meta] = {
-      arrow_type = {
-        [Layer.key.refines] = {
-          record,
-        },
+layer [Layer.key.labels] = {
+  ["cosy.formalism.graph"] = true,
+}
+local _ = Layer.reference "cosy.formalism.graph"
 
-        [Layer.key.meta] = {
-          tags = {
-            vertex = {
-              value_type      = _ [Layer.key.meta].vertex_type,
-              value_container = _.vertices,
-            },
-          },
-        },
-      },
+-- Vertices are empty in base graph.
+local vertex_type = {}
+
+-- Arrows are records with only one predefined field: `vertex`.
+-- It points to the destination of the arrow, that must be a vertex of the
+-- graph.
+local arrow_type  = {
+  [Layer.key.refines] = {
+    record,
+  },
+  [Layer.key.meta   ] = {
+    vertex = {
+      value_type      = _ [Layer.key.meta].vertex_type,
+      value_container = _.vertices,
     },
-    arrows = {
+  },
+}
+
+-- Edges have no label in base graph.
+-- They only contain zero to several arrows. The arrow type is defined for
+-- each edge type.
+-- The `default` key states that all elements within the `arrows` container
+-- are of type `arrow_type`.
+local edge_type   = {
+  [Layer.key.meta] = {
+    arrow_type = arrow_type,
+  },
+  arrows = {
+    [Layer.key.refines] = {
+      collection,
+    },
+    [Layer.key.meta] = {
+      value_type = _ [Layer.key.meta].edge_type [Layer.key.meta].arrow_type,
+    },
+    [Layer.key.default] = {
       [Layer.key.refines] = {
-        collection,
-      },
-      [Layer.key.meta] = {
-        value_type = _ [Layer.key.meta].edge_type[Layer.key.meta].arrow_type,
-      },
-      [Layer.key.default] = {
-        [Layer.key.refines] = {
-          _ [Layer.key.meta].edge_type[Layer.key.meta].arrow_type,
-        },
+        _ [Layer.key.meta].edge_type [Layer.key.meta].arrow_type,
       },
     },
   },
 }
 
+-- A graph defines the type of its vertices, and the type of its edges.
+layer [Layer.key.meta] = {
+  vertex_type = vertex_type,
+  edge_type   = edge_type,
+}
+
+-- A graph contains a collection of vertices.
+-- The `default` key states that all elements within the `vertices` container
+-- are of type `vertex_type`.
 layer.vertices = {
   [Layer.key.refines] = {
     collection,
@@ -67,6 +91,9 @@ layer.vertices = {
   },
 }
 
+-- A graph contains a collection of edges.
+-- The `default` key states that all elements within the `edges` container
+-- are of type `edge_type`.
 layer.edges = {
   [Layer.key.refines] = {
     collection,
