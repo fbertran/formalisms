8b6101ee048caf59f999ec4db2dad37139967e81
Alban Linard <alban@linard.fr>
2016-02-01 11:44:23 +0100

    Implement checks on graph.unique-edges.

--- src/cosy/formalism/graph/unique-edges.lua
+++ src/cosy/formalism/graph/unique-edges.lua
@@ -1,33 +1,68 @@
-local Layer             = require "layeredata"
-local hyper_multi_graph = require "formalisms.hyper_multi_graph"
-local layer             = Layer.new {
-  name = "hyper graph",
+local Layer = require "layeredata"
+local graph = require "cosy.formalism.graph"
+local layer = Layer.new {
+  name = "cosy.formalism.graph.unique-edges",
 }
 
--- Formalism of a Hyper Graph
--- ===========================
+-- Graph with unique edges
+-- =======================
 --
--- We describe here what is a Hyper Graph
+-- This formalism corresponds to the removal of the multigraph part of graphs.
+-- There are no two edges linking the same vertices.
 --
--- A Hyper Graph extends definition of Hyper and Multi Graph with one constraint, we cannot have identical edges.
---
--- Formalism of Hyper and Multi Graph is describe [here](./hyper_multi_graph.html)
---
--- For more information see [here](https://en.wikipedia.org/wiki/Hypergraph)
+-- For more information see [here](https://en.wikipedia.org/wiki/Multigraph)
 
-layer[Layer.key.refines] = {
-  hyper_multi_graph
+layer [Layer.key.labels] = {
+  ["cosy.formalism.graph.unique-edges"] = true,
+}
+local _ = Layer.reference "cosy.formalism.graph.unique-edges"
+
+layer [Layer.key.refines] = {
+  graph,
 }
 
-layer[Layer.key.meta] = {
-  edges = {
-    [Layer.key.checks] = {
-      function ()
-        -- TODO
-        -- check identical edges
-      end,
-    },
-  },
+-- We add a check function to the `edges` container, that iterates through it,
+-- and verifies that there are no two edges with the same vertices.
+--
+-- FIXME: this is very inefficient, as it performs iteration on the whole
+-- collection each time it is modified.
+layer.edges [Layer.key.checks] = {
+  ["cosy.formalism.graph.unique-edges"] = function (proxy)
+    local edges = {}
+    for _, edge in pairs (_.edges) do
+      local vertices = {}
+      for _, arrow in pairs (edge.arrows) do
+        vertices [arrow.vertex] = true
+      end
+      edges [edge] = vertices
+    end
+    for edge_l, vertices_l in pairs (edges) do
+      for edge_r, vertices_r in pairs (edges) do
+        if vertices_l ~= vertices_r then
+          local found_l, found_r = false, false
+          for vertex in pairs (vertices_l) do
+            if vertices_r [vertex] then
+              found_l = true
+              break
+            end
+          end
+          for vertex in pairs (vertices_r) do
+            if vertices_l [vertex] then
+              found_r = true
+              break
+            end
+          end
+          if found_l and found_r then
+            Layer.coroutine.yield ("cosy:formalism:graph:unique-edges:illegal", {
+              proxy     = proxy,
+              container = _.edges,
+              edges     = { edge_l, edge_r },
+            })
+          end
+        end
+      end
+    end
+  end,
 }
 
 return layer
