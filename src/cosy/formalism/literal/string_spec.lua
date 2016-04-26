34e0d6d82ca88c56c34d7255a37a6381fa01285c
Nizar Hdadech <hdadech.nizar@gmail.com>
2016-04-26 18:34:13 +0200

    busted test that fails (assert_is_not_nil and is_nil gives the same result)

---
+++ src/cosy/formalism/literal/string_spec.lua
@@ -0,0 +1,41 @@
+-- These lines are required to correctly run tests.
+require "busted.runner" ()
+
+local Layer = require "cosy.formalism.layer"
+
+describe ("Formalism literal.literal", function ()
+
+  it ("can be loaded", function ()
+    local _ = Layer.require "cosy/formalism/literal.string"
+  end)
+
+  describe ("with type information", function ()
+
+    it ("right type", function ()
+      local string = Layer.require "cosy/formalism/literal.string"  
+      local layer      = Layer.new {
+        name = "layer",
+        data = {
+          [Layer.key.refines] = { string },
+          value = "test",  
+        },
+      }
+      Layer.Proxy.check (layer)
+      assert.is_nil (Layer.messages (layer) ())
+    end)
+
+      it ("forbids a different type", function ()
+      local string = Layer.require "cosy/formalism/literal.string"  
+      local layer      = Layer.new {
+        name = "layer",
+        data = {
+          [Layer.key.refines] = { string },
+          value = 12,  
+        },
+      }
+      Layer.Proxy.check (layer)
+      assert.is_nil (Layer.messages (layer) ())
+    end)
+	end)
+
+end)
