3b20eec70def89ee434dbf0975499864f5f54f73
Alban Linard <alban@linard.fr>
2016-05-23 14:40:27 +0200

    Simplify check_container.

--- src/cosy/formalism/data/check_container.lua
+++ src/cosy/formalism/data/check_container.lua
@@ -1,19 +1,5 @@
 return function (Layer, check_container)
 
-  local function is_prefix (lhs, rhs)
-    lhs = Layer.hidden [lhs]
-    rhs = Layer.hidden [rhs]
-    if lhs.layer ~= rhs.layer then
-      return false
-    end
-    for i = 1, math.min (#lhs.keys, #rhs.keys) do
-      if lhs.keys [i] ~= rhs.keys [i] then
-        return false
-      end
-    end
-    return true
-  end
-
   check_container.apply = function (value, container, where)
     if getmetatable (value) ~= Layer.Proxy then
       Layer.coroutine.yield (where.prefix .. ".not-proxy", {
@@ -25,10 +11,7 @@ return function (Layer, check_container)
     else
       for _, element in Layer.pairs (container) do
         if element <= value or value <= element then
-        -- for dependency in Layer.Proxy.dependencies (element) do
-        --   if is_prefix (dependency, value) then
-            return
-          -- end
+          return
         end
       end
       Layer.coroutine.yield (where.prefix .. ".not-contained", {
