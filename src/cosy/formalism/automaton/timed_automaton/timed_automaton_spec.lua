c36d97729992f4c2f12d49d61b930866899b5bfe
Nizar_Hdadech <hdadech.nizar@gmail.com>
2016-05-31 19:08:04 +0200

    adding lpeg to the test environment + update of some  unit tests

---
+++ src/cosy/formalism/automaton/timed_automaton/timed_automaton_spec.lua
@@ -0,0 +1,48 @@
+if #setmetatable ({}, { __len = function () return 1 end }) ~= 1 then
+  require "compat52"
+end
+--These lines are required to correctly run tests.
+require "busted.runner" ()
+
+local Layer = require "layeredata"
+
+describe ("Formalism timed_automaton", function ()
+
+  it ("can be loaded", function ()
+
+    local _ = Layer.require "cosy/formalism/automaton/timed_automaton"
+  end)
+
+  describe ("with type information", function ()
+
+    it ("parsing!!!!", function ()
+      local timed_automaton = Layer.require "cosy/formalism/automaton/timed_automaton"
+
+      local refines =  Layer.key.refines
+
+
+     local layer=Layer.new {
+     name="timed_automaton",
+     data = {
+       [refines] = {
+         timed_automaton,
+       }
+      }
+     }
+--layer.invariants={}
+
+layer.clocks.c1={value="x1"}
+print(layer.clocks.c1.value)
+local op=timed_automaton.parser("INF(ADD(layer.clocks.c1,2),1)",layer)
+
+--print(op.operands[1].operator)
+print(timed_automaton.printer(op))
+  
+
+
+     -- Layer.Proxy.check_all (layer)
+     -- assert.is_nil (Layer.messages[layer])
+    end)
+	end)
+
+end)
