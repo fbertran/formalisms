ca3510824c0b928ba29b8a5bd9fd95d2527a5eb6
Nizar_Hdadech <hdadech.nizar@gmail.com>
2016-06-08 14:08:36 +0200

    deleting test_timed+test_automaton

--- src/cosy/formalism/automaton/timed_automaton/test_timed.lua
+++
@@ -1,5 +0,0 @@
-local Layer = require "layeredata"
-
-local timed_automaton = Layer.require "cosy/formalism/automaton/timed_automaton"
-
-print(timed_automaton[meta].state_type[meta][record].initial.value_type)
