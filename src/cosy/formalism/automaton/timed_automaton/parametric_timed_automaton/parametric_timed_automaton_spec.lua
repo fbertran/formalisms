493f79d259771a6c76461cc71f596bc5b07da264
Nizar_Hdadech <hdadech.nizar@gmail.com>
2016-06-09 17:21:16 +0200

    update of pta_spec

--- src/cosy/formalism/automaton/timed_automaton/parametric_timed_automaton/parametric_timed_automaton_spec.lua
+++ src/cosy/formalism/automaton/timed_automaton/parametric_timed_automaton/parametric_timed_automaton_spec.lua
@@ -1,7 +1,10 @@
+if #setmetatable ({}, { __len = function () return 1 end }) ~= 1 then
+  require "compat52"
+end
 --These lines are required to correctly run tests.
 require "busted.runner" ()
 
-local Layer = require "cosy.formalism.layer"
+local Layer = require "layeredata"
 
 describe ("Formalism timed_automaton", function ()
 
