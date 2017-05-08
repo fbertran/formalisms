7e7f8f639e12b340df4170b7978c5d28a707683a
Lars Gabriel Annell Rydenvald <lars.annell@etu.unige.ch>
2017-05-08 14:56:40 +0200

    add some parsing prototype

--- src/parsertest.lua
+++ src/parsertest.lua
@@ -3,6 +3,7 @@ local lpeg = require "lpeg"
 
 local collection = Layer.require "data.collection"
 
+local refines = Layer.key.refines
 
 
 local value_types = {
@@ -10,13 +11,8 @@ local value_types = {
   ["boolean"] = lpeg.C(lpeg.P("true") + lpeg.P("false"))
 }
 
-local and_expression = Layer.require "expression.and_expression"
+local addition_expression = Layer.require "expression.addition_expression"
 local mul_expr = Layer.require "expression.multiplication_expression"
-
-local test = {
-  and_expression,
-  mul_expr
-}
 
 -- http://leafo.net/guides/parsing-expression-grammars.html
 local function node(p)
@@ -25,20 +21,20 @@ local function node(p)
   end
 end
 
-local factor
 local white = lpeg.S(" \t") ^ 0
 
-local function make_nodes()
+--[[local function make_nodes()
 
   local count = 0
-  local obj = {
-    "0",
-  }
+  local obj = { }
+  obj[1] = "asdf"
+  obj["asdf"] = lpeg.V("asdf")
 
+  local expr
 
   for _, v in pairs(test) do
     local tab = Layer.new {}
-    local expr
+
     tab [Layer.key.refines] = { v }
 
     local operand = white * value_types[tab.operator.operands[collection].value_type]
@@ -47,7 +43,7 @@ local function make_nodes()
     if factor == nil then
       factor = node(operand * op * operand + operand)
     else
-      factor = node(operand * op * operand + operand) + factor
+      factor = node(operand * op * operand + operand)
     end
 
     count = count + 1
@@ -59,10 +55,58 @@ local function make_nodes()
     end
   end
 end
+]]
 
+local tab = Layer.new {}
+tab [refines] = { mul_expr }
 
-make_nodes()
+local operand = white * value_types[tab.operator.operands[collection].value_type]
+local mult = white * lpeg.C(lpeg.S(tab.operator.operator))
 
-for _, v in pairs(factor:match("3 * 5 * 2")) do
-  print(v)
+local tab2 = Layer.new {}
+tab2 [refines] = { addition_expression }
+
+-- operand = value_types[tab2.operator.operands[collection].value_type]
+local add = white * lpeg.C(lpeg.S(tab2.operator.operator))
+
+--[[
+  input          -> exp
+  exp            -> addition | multiplication | number
+  addition       -> (multiplication | number) '+' exp
+  multiplication -> number '*' (multiplication | number)
+
+  This grammar has priority for multiplication
+]]
+local function make_nodes()
+  return lpeg.P({
+    "input",
+    input = lpeg.V("exp") * -1,
+    exp = lpeg.V("addition") + lpeg.V("addition") + operand,
+    addition = node((lpeg.V("multiplication") + operand) * add * lpeg.V("exp")),
+    multiplication = node(operand * mult * (lpeg.V("multiplication") + operand))
+  })
 end
+
+local str = ""
+
+local function printr(t, f)
+  if f then
+    str = str .. "{"
+  else
+    str = str .. " {"
+  end
+  for _, v in pairs(t) do
+    if type(v) == "table" then
+      printr(v, false)
+    else
+      str = str .. " " .. v
+    end
+  end
+  str = str .. " }"
+end
+
+local original = "10 * 3 + 5 * 2 + 2"
+
+printr(make_nodes():match(original), true)
+print("original: " .. original)
+print("parsed result: " .. str)
