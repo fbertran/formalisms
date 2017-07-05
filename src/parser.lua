08357b8708ad1ad32029afa5e60c924d936f9dea
Lars Gabriel Annell Rydenvald <lars.annell@etu.unige.ch>
2017-07-05 16:07:51 +0200

    change some stuff + benchmarks

--- src/parser.lua
+++ src/parser.lua
@@ -1,6 +1,20 @@
 -- local lp = require("lpeg")
 -- local lp = require("lpeglabel")
 
+
+-- Builds a grammar from operators
+-- Example grammar with number, plus, minus, multiplication and division operators
+
+-- the numbers following "prefix" just mean the priority of the operator
+-- This is why the operators which have the same priority are on the same "line"
+
+-- exp      -> prefix11
+-- prefix11 -> prefix12 * '+' * (prefix11 + prefix12) + prefix12 * '-' * (prefix11 + prefix12) + prefix12
+-- prefix12 -> prefix15 * '*' * (prefix12 + prefix15) + prefix15 * '/' * (prefix12 + prefix15) + prefix15
+-- prefix15 -> [0-9]+
+
+local pprint = require "pprint"
+
 local lp
 local prefix = "prefix"
 
@@ -13,18 +27,23 @@ end
 local op_map = {}
 
 local str = ""
-local function table_to_string (t, f)
+local function table_to_string (t, f, print_keys)
   if f then
     str = str .. "{"
   else
     str = str .. " {"
   end
   if type(t) == "table" then
-    for _, v in pairs(t) do
+    for k, v in pairs(t) do
       if type(v) == "table" then
-        table_to_string(v, false)
+        str = str .. " " .. k .. ": "
+        table_to_string(v, false, print_keys)
       else
-        str = str .. " " .. tostring(v)
+        if print_keys then
+          str = str .. " ".. k .. ": " .. tostring(v) .. ", "
+        else
+          str = str .. " " .. tostring(v)
+        end
       end
     end
   else
@@ -35,10 +54,10 @@ end
 
 local function ptable(t)
   str = ""
-  table_to_string(t, true)
+  table_to_string(t, true, true)
   print(str)
 end
--- assert(false)
+
 
 local white = lp.S(" \t") ^ 0
 
@@ -53,7 +72,7 @@ local op_plus = {
   operator   = "+",
   value_type = "",
   type       = "binary",
-  left_assoc = true
+  -- left_assoc = true
 }
 
 local op_minus = {
@@ -76,7 +95,7 @@ local op_multiplication = {
   operator   = "*",
   value_type = "",
   type       = "binary",
-  left_assoc = true
+  -- left_assoc = true
 }
 
 local op_division = {
@@ -200,18 +219,18 @@ local expression = {
   r_division       = op_division,
   r_plus           = op_plus,
   r_minus          = op_minus,
-  r_exp            = exp_op,
-  -- -- -- r_variable       = op_variable,
-  -- -- -- -- -- r_modulo         = op_modulo,
-  -- r_if             = op_ternary,
-  r_not            = op_unary,
-  r_negative       = op_negative,
-  r_ternary        = op_ternary2,
-  -- -- -- r_equal          = op_equals,
-  -- -- r_less_equal     = op_le,
-  r_greater_equal  = op_ge,
-  -- -- r_or             = op_or,
-  -- -- r_and            = op_and,
+  -- r_exp            = exp_op,
+  -- r_variable       = op_variable,
+  -- r_modulo         = op_modulo,
+  r_if             = op_ternary,
+  -- r_not            = op_unary,
+  -- r_negative       = op_negative,
+  -- r_ternary        = op_ternary2,
+  -- -- -- -- -- r_equal          = op_equals,
+  -- -- -- -- r_less_equal     = op_le,
+  -- -- r_greater_equal  = op_ge,
+  -- -- -- -- r_or             = op_or,
+  -- -- -- -- r_and            = op_and,
   r_sum            = op_sum,
   -- -- r_pi             = op_pi,
   -- r_assignment     = op_assignment,
@@ -232,55 +251,50 @@ end
 -- else when iterating over it with the `pairs` function
 -- we get the elements in the wrong order (they are sorted alphabetically)
 local function sort_by_priority(t)
-  local nt = {}
+  local nt = { }
   local i  = 1
 
-  for _, v in pairs (t) do
+  for _, v in pairs(t) do
     nt[i] = v
     i = i + 1
   end
 
-  table.sort (nt, function (op1, op2)
+  table.sort(nt, function (op1, op2)
       return op1.priority > op2.priority
   end)
 
   return nt
 end
 
-
-local function node_postfix (p)
-
+local function node_postfix(p)
   local function construct(...)
     local list = ...
-
     local function rec(n, t)
-      if n == tlen(list) then
-        return { list[n] }
+      if n == 1 then
+        return list[1]
       else
-        t = { list[n], rec(n + 1, { t }) }
+        t = { op = list[n], left = rec(n - 1, { t }) }
       end
       return t
     end
 
-    return rec(1, { })
+    return rec(tlen(list), { })
   end
-
-  return p / function (value, ...)
-    return { value, construct({ ... }) }
+  return p / function(...)
+    return construct({ ... })
   end
 end
 
-
 local function node_prefix (p)
   return p / function (op, right)
-    return { op, right }
+    return { op = op, right = right,  op_type = "prefix" }
   end
 end
 
 
 local function ternary_node (p)
   return p / function (left, op1, middle, op2, right)
-    return { left, op1, middle, op2, right}
+    return { left = left, op1 = op1, middle = middle, op2 = op2, right = right, op_type = "ternary"}
   end
 end
 
@@ -299,13 +313,15 @@ local function nary_node (p)
       end
     end
 
-    return { op, arglist }
+    return { op = op, operands = arglist, op_type = "nary" }
   end
 end
 
 local function find_operator(t)
+  ptable(t)
   local ctr = 1
   for _, v in ipairs(t) do
+    print(v)
     if type(v) == "string" and op_map[v] ~= nil then
       return ctr
     end
@@ -316,30 +332,46 @@ end
 local function lassoc(p, _op)
 
   local function fn(left, op, right)
-    if type(right) == "table" then
-      if tlen(right) ~= 3 then
+    -- second condition is to make sure that we are not "stealing"
+    -- anything from operators that have the same priority but
+    -- are not left associative
+    if type(right) == "table" and op == _op.operator then
+      if tlen(right) ~= 4 then
         -- it's not a binary operator, so left / right associativity doesn't apply
-        return { left, op, right }
+        -- This can also mean that there is a parenthesis
+        return { left = left, op = op, right = right, op_type = _op.type }
       end
 
       -- we have to take precedence into account
-      if op_map[right[find_operator(right)]].priority > _op.priority then
-        return { left, op, right }
+      -- if op_map[right[find_operator(right)]].priority > _op.priority then
+      if op_map[right["op"]].priority > _op.priority then
+        return { left = left, op = op, right = right, op_type = _op.type }
       end
 
-      return fn({ left, op, right[1] }, right[2], right[3])
+      -- Recursion needed to move the operands to make the operator left-associative
+      return fn(
+        {
+          left = left, op = op, right = right["left"], op_type = _op.type
+        },
+        right["op"],
+        right["right"]
+      )
     else
       if type(left) == "table" then
-        left = fn(left[1], left[2], left[3])
+        if tlen(left) ~= 4 then
+          -- it's not a binary operator, so left / right associativity doesn't apply
+          -- This can also mean that there is a parenthesis
+          return { left = left, op = op, right = right, op_type = _op.type }
+        end
+        left = fn(left["left"], left["op"], left["right"])
       end
 
-      return { left, op, right }
+      return { left = left, op = op, right = right, op_type = _op.type }
     end
   end
 
   return p / fn
 end
-
 
 -- Hashmap for the patterns so we don't need
 -- a ton of `if - else if - else` statements
@@ -353,7 +385,9 @@ local patterns = {
     if operator.left_assoc then
       pattern = lassoc(pattern, operator)
     else
-      pattern = lp.Ct(pattern)
+      pattern = pattern / function (left, op, right)
+        return { left = left, op = op, right = right, op_type = operator.type }
+      end
     end
 
     return pattern
@@ -412,7 +446,6 @@ local function add_expr(grammar, expr, expr_ref)
   return grammar
 end
 
-
 -- Builds the grammar up from the bottom,
 -- i.e. we start with the highest priority operators
 -- and end with the lowest ones
@@ -426,7 +459,7 @@ local function build_grammar(expr)
 
   local old_priority = expr[1].priority
 
-  local op_table = {}
+  local op_table = { }
   local counter = 1
   local second_counter = 0
 
@@ -480,11 +513,11 @@ local function build_grammar(expr)
 
   for k = 1, tlen(op_table), 1 do
       str = ""
-      table_to_string(op_table[k])
+      table_to_string(op_table[k], true, true)
       print(str)
   end
 
-  grammar["prefix15"] = grammar["prefix15"] + ("(" * white * lp.V("axiom") * white * ")")
+  grammar["prefix15"] = grammar["prefix15"] + ("(" * white * lp.Ct(lp.V("axiom")) * white * ")")
   grammar.axiom = lp.V(prefix .. op_table[tlen(op_table)][1].priority)
 
   return lp.P(grammar)
@@ -494,12 +527,13 @@ end
 local g4 = build_grammar(expression)
 
 local has_input = arg[2] ~= nil
+
 if has_input then
   local result = g4:match(arg[2])
 
-  if arg[1] and arg[1] == "lulpeg" then
-    lp.pprint(g4)
-  end
+  -- if arg[1] and arg[1] == "lulpeg" then
+  --   lp.pprint(g4)
+  -- end
 
   str = ""
   if type(result) == "table" then
@@ -522,6 +556,7 @@ while input ~= "d" do
 
   if type(result) == "table" then
     ptable(result)
+    pprint(result)
   else
     print(result)
   end
