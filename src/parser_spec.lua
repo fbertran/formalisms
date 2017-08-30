014257ea809d9be3bb81b0e5044ef355a32732ac
Lars Gabriel Annell Rydenvald <lars.annell@etu.unige.ch>
2017-08-30 13:31:44 +0200

    update article and parser tests

--- src/parser_spec.lua
+++ src/parser_spec.lua
@@ -518,13 +518,13 @@ describe("parser", function ()
 
     it("parses correctly, input = " .. v.expression_string, function ()
       local cmpres = deepcompare(result, v.expected, true)
-      if not cmpres then
-        print("input", v.expression_string)
-        print("result:")
-        pprint(result)
-        print("expected")
-        pprint(v.expected)
-      end
+      -- if not cmpres then
+      --   print("input", v.expression_string)
+      --   print("result:")
+      --   pprint(result)
+      --   print("expected")
+      --   pprint(v.expected)
+      -- end
       assert.is_true(cmpres)
     end)
   end
